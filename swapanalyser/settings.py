a081d7efbd4bca3e97b3a4a2167f253452334bdc
David Clamp <dclamp@yahoo.com>
2026-03-05 08:17:59 +0000

    Readiness

--- swapanalyser/settings.py
+++ swapanalyser/settings.py
@@ -90,10 +90,10 @@ WSGI_APPLICATION = 'swapanalyser.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
 
+DATABASES = {
+    'default': dj_database_url.parse(str(os.environ.get("DATABASE_URL", "")))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
