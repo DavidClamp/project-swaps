339a639e6d06d1c31cd1d69b20b8c0b2fc30c974
David Clamp <dclamp@yahoo.com>
2026-03-05 14:10:12 +0000

    Install Allauth. Still in preping workspace

--- swapanalyser/urls.py
+++ swapanalyser/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from swaps.views import my_swaps
 
 urlpatterns = [
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path('', my_swaps, name='home'), 
     path('swaps/', my_swaps, name='swaps'),
