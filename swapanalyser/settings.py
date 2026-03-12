437fa6ff9bfd743b40aa444c69b1c852f53a5f17
David Clamp <dclamp@yahoo.com>
2026-03-12 15:16:53 +0000

    Debug to false, heroku test

--- swapanalyser/settings.py
+++ swapanalyser/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Cleaner DEBUG check to prevent accidental True in production
-DEBUG = True
+DEBUG = False
 # DEBUG = True
 
 ALLOWED_HOSTS = [
@@ -46,7 +46,6 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
 ]
-
 
 # Application definition
 
