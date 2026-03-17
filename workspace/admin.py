dfa82374267821f989c481745f64028dc4ab2867
David Clamp <dclamp@yahoo.com>
2026-03-17 13:07:31 +0000

    Update Admin with workspace models

--- workspace/admin.py
+++ workspace/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import HistoricalRate, Trade, Profile
+
+@admin.register(HistoricalRate)
+class HistoricalRateAdmin(admin.ModelAdmin):
+    list_display = ('date', 'index_name', 'tenor', 'rate')
+    list_filter = ('index_name', 'tenor')
+
+@admin.register(Trade)
+class TradeAdmin(admin.ModelAdmin):
+    list_display = ('trade_id', 'user', 'strategy', 'ticker', 'last_npv')
+    search_fields = ('trade_id', 'ticker')
+
+ # Toggle PRO status
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'is_subscriber', 'stripe_customer_id')
+    list_editable = ('is_subscriber',) 
+    
--- workspace/models.py
+++ workspace/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class HistoricalRate(models.Model):
     """Global Market Data Store: SOFR, SONIA, TONA, EURIBOR, etc."""
@@ -65,3 +67,10 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
+     
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """Automatically creates a Profile whenever a new User is registered."""
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
